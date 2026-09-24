df5f3d1af8a27b3d68e6b8aa3061f46161094aa7
Kev <kevin277507@gmail.com>
2026-09-24 18:20:45 -0500

    dto reserva y transporte creados y terminados

--- TallerApiViajes/src/main/java/BussisnesCatLayer/dto/ReservaDTO.java
+++ TallerApiViajes/src/main/java/BussisnesCatLayer/dto/ReservaDTO.java
@@ -1,21 +1,22 @@
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import java.util.LocalDateTime
 
 @Data
 @AllArgsConstructor
 @Schema(descripcion = "informacion de la reserva")
 public class ReservaDTO{
-
+    @Schema(description = "ID único de la reserva", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
     private Long idReserva;
-
-    private DateTime fecha;
-
+    @Schema(description = "fecha de la reserva", example = "2026-09-30T15:45:0", required = true)
+    private LocalDateTime fecha;
+    @Schema(description = "Estado de la reserva (activa, cancelada, pendiente)", example = "activa", required = true)
     private String estado;
-
+    @Schema(description = "numero de personas que se registran a la reserva", example = "4", required = true)
     private int numeroPersonas;
-
-    private ViajeEntity viaje;
-
-    private ClienteEntity cliente;
+    @Schema(description = "ID del viaje asignado", example = "4", required = true)
+    private Long idViaje;
+    @Schema(description = "ID del cliente asignado a la reserva", example = "2", required = true)
+    private Long idCliente;
 }
---
+++ TallerApiViajes/src/main/java/BussisnesCatLayer/dto/TransporteDTO.java
@@ -0,0 +1,23 @@
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.LocalDateTime
+
+@Data
+@AllArgsConstructor
+@Schema(descripcion = "informacion del transporte")
+public class TransporteEntity {
+    @Schema(description = "ID único del transporte", example = "123", accessMode = Schema.AccessMode.READ_ONLY)
+    private Long idTransporte;
+    @Schema(description = "Nombre de la compañia del transporte", example = "Coochoferes", required = true)
+    private String compania;
+    @Schema(description = "Horario de salida del transporte", example = "2026-10-07T11:30:00", required = true)
+    private LocalDateTime horario;
+    @Schema(description = "Duracion del viaje en horas", example = "10", required = true)
+    private int duracion;
+    @Schema(description = "Tipo de clase que se toma el servicio como primera, clase ejecutiva, etc", example = "primera clase", required = true)
+    private String claseServicio;
+    @Schema(description = "ID del viaje asignado", example = "4", required = true)
+    private Long idViaje;
+
+}
