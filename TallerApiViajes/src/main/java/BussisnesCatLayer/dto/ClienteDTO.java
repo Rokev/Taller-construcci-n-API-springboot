5c652a618ef5c716420f831bf9d97059c84c23e9
nataly <n4taly.morales24040@gmail.com>
2026-09-24 17:58:44 -0500

    documentacion DTO cliente

--- TallerApiViajes/src/main/java/BussisnesCatLayer/dto/ClienteDTO.java
+++ TallerApiViajes/src/main/java/BussisnesCatLayer/dto/ClienteDTO.java
@@ -6,14 +6,17 @@ import lombok.Data;
 @AllArgsConstructor
 @Schema(descripcion = "informacion del cliente")
 public class ClienteDTO {
-
+    @Schema(description = "id unico de cliente", example = "154", accessMode = Schema.AccessMode.READ_ONLY)
     private Long idCliente;
 
+    @Schema(description = "Nombre del cliente", example = "maria", required = true, maxLength = 50)
     private String nombre;
 
+    @Schema(description = "email", example = "an12@gmail.com", required = true)
     private String email;
 
+    @Schema(description = "lugar de residencia del cliente", example = "calle 35 casa n34-57", required = true)
     private String direccion;
 
-    private List<ReservaEntity> reservas;
+
 }
