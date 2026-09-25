c4ebd4b453496f94f280b6959d2b6d12b94cfff0
Kev <kevin277507@gmail.com>
2026-09-24 19:53:36 -0500

    mapper creado, tiene de dto a entidad y de entidad a dto

---
+++ TallerApiViajes/src/main/java/persistanceLayer/mapper/mapper.java
@@ -0,0 +1,33 @@
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface mapper {
+
+	ViajeDTO toDto(ViajeEntity entity);
+
+	@Mapping(target = "fechasDisponibles", ignore = true)
+	@Mapping(target = "reservas", ignore = true)
+	@Mapping(target = "transportes", ignore = true)
+	ViajeEntity toEntity(ViajeDTO dto);
+
+	@Mapping(target = "idViaje", source = "viaje.idViaje")
+	@Mapping(target = "idCliente", source = "cliente.idCliente")
+	ReservaDTO toDto(ReservaEntity entity);
+
+	@Mapping(target = "fecha", ignore = true)
+	@Mapping(target = "viaje", ignore = true)
+	@Mapping(target = "cliente", ignore = true)
+	ReservaEntity toEntity(ReservaDTO dto);
+
+	ClienteDTO toDto(ClienteEntity entity);
+
+	@Mapping(target = "reservas", ignore = true)
+	ClienteEntity toEntity(ClienteDTO dto);
+
+	@Mapping(target = "idViaje", source = "viaje.idViaje")
+	TransporteDTO toDto(TransporteEntity entity);
+
+	@Mapping(target = "viaje", ignore = true)
+	TransporteEntity toEntity(TransporteDTO dto);
+}
